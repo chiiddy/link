9f1c19340148f679da5658b402d971ff8f5d6a40
chidicolly11@gmail.com <chidicolly11@gmail.com>
2023-07-25 14:34:26 +0100

    officeNewss

--- src/home.jsx
+++ src/home.jsx
@@ -39,10 +39,10 @@ export default function Home() {
 
     emailjs
       .sendForm(
-        "service_9pznuju",
-        "template_18m60wc",
+        "service_69c4uap",
+        "template_k9v86d4",
         form.current,
-        "C_wSoAeT9Ri-giwyP"
+        "9Ok1ojMF6V7OqJ3wc"
       )
       .then((res) => {
         e.target.reset();
